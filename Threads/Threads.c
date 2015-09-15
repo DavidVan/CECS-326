d9ef7072aa9efd0f63bdf61ef353591d22082357
David Van <David@DavidVan.net>
2015-09-14 17:50:39 -0700

    Finished!

--- Threads/Threads.c
+++ Threads/Threads.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 
 static void *child(void *ignored) {
-	sleep(3);
-	printf("Child is done sleeping 3 seconds.\n");
+	while(1) { // Infinite loop.
+		printf("Child will start sleeping for 5 seconds.\n");
+		sleep(5);
+		printf("Child is done sleeping 5 seconds.\n");
+	}
 	return NULL;
 }
 
@@ -12,13 +15,25 @@ int main(int argc, char *argv[]) {
 
 	pthread_t child_thread;
 	int code;
-	code = pthread_create(&child_thread, NULL, child, NULL);
+	code = pthread_create(&child_thread, NULL, child, NULL); // Starts the child thread.
 
 	if (code) {
 		fprintf(stderr, "pthread_create failed with code %d\n", code);
 	}
 
-	sleep(5);
-	printf("Parent is done sleeping 5 seconds.\n");
+	char c;
+
+	while(1) {
+		// Get user input.
+		c = getchar();
+		// Since we only need the enter key, this should work.
+		if (c == '\n') {
+			break;
+		}
+	}
+
+	pthread_cancel(child_thread); // This kills the thread. ;-;
+
+	printf("Child thread killed.\n");
 	return 0;
 }
